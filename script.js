ede7f6c38e508388bdd35901246a236dc57abd20
goldigdo5 <goldigd05>
2025-05-21 21:38:49 +0530

    Initial commit with portfolio files

---
+++ script.js
@@ -0,0 +1,13 @@
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  color: 0x2196f3,
+  backgroundColor: 0x000000,
+  points: 14.0,
+  maxDistance: 22.0,
+  spacing: 18.0
+});
